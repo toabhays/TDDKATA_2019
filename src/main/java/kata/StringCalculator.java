83edca917aaf954060cb21e6fe9d029fcbc31699
Abhay.Singh <abhay.singh@eclinicalworks.com>
2019-07-27 06:30:27 -0400

    Implementation and refactor is done for null, empty, 1 and "1,2"

--- src/main/java/kata/StringCalculator.java
+++ src/main/java/kata/StringCalculator.java
@@ -10,6 +10,18 @@ public class StringCalculator {
 		 * (for an empty string it will return 0)
 		 */
 
+		if(numbers != null && numbers.trim().length() > 0)
+		{
+			String[] num = numbers.split(",");
+
+			if(num.length == 1)
+			{
+				return Integer.parseInt(numbers);
+			} else {
+				return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+			}
+		}
+
 		return 0;
 	}
 }
