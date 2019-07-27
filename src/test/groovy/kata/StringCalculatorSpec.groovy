57d2105715b30d7c28b29d39ad843561f3321bc7
Abhay.Singh <abhay.singh@eclinicalworks.com>
2019-07-27 06:24:22 -0400

    Test case ready for null, empty, 1 and "1,2"

--- src/test/groovy/kata/StringCalculatorSpec.groovy
+++ src/test/groovy/kata/StringCalculatorSpec.groovy
@@ -10,4 +10,19 @@ class StringCalculatorSpec extends Specification {
 		then: "it should written sum of two numbers"
 			result == 0;
 	}
+
+	def "Test case for different numbers" () {
+		given: "number"
+		when: "add(String numbers) is called"
+			def result = new StringCalculator().add(numbers);
+		then: "it should written sum of two numbers"
+			result == excpectedResult;
+		where : "Provided two numbers for numbers and excpectedResult"
+				numbers		  	| excpectedResult
+				null   			| 0
+				""   			| 0
+				"1"   			| 1
+				"1,2"   		| 3
+	}
+
 }
